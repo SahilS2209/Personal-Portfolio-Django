ea6059399b81f0e8051cbf8f208c4ad79e17049f
Sahil Shrivastav <shrivastavsahil310@gmail.com>
2022-11-03 08:04:46 +0530

    commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn digi_resume.wsgi:application --log-file -
---
+++ portfolio/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .models import (
+		UserProfile,
+		Portfolio,
+		Testimonial,
+		Certificate
+	)
+
+from django.views import generic
+
+
+from . forms import ContactForm
+
+
+class IndexView(generic.TemplateView):
+	template_name = "portfolio/index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		
+		testimonials = Testimonial.objects.filter(is_active=True)
+		certificates = Certificate.objects.filter(is_active=True)
+		portfolio = Portfolio.objects.filter(is_active=True)
+		
+		context["testimonials"] = testimonials
+		context["certificates"] = certificates
+		context["portfolio"] = portfolio
+		return context
+
+
+class ContactView(generic.FormView):
+	template_name = "portfolio/contact.html"
+	form_class = ContactForm
+	success_url = "/"
+	
+	def form_valid(self, form):
+		form.save()
+		messages.success(self.request, 'Thank you for contacting.')
+		return super().form_valid(form)
+
+
+class PortfolioView(generic.ListView):
+	model = Portfolio
+	template_name = "portfolio/portfolio.html"
+	paginate_by = 10
+
+	def get_queryset(self):
+		return super().get_queryset().filter(is_active=True)
+
+
+class PortfolioDetailView(generic.DetailView):
+	model = Portfolio
+	template_name = "portfolio/portfolio-detail.html"
+ 
+
+def about(request):
+    return render(request, 'about.html')
